bbcff125c1ced77cedf0f5b289d61b6b3f877a9e
Srharahaad <sunshineflash.mlp@gmail.com>
2022-10-12 10:56:53 +0200

    Premier script

---
+++ primer_script.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+echo "pour l'année 2016" > sortie.txt
+grep "Location" 2016*.ann | wc -l >> sortie.txt
+echo "pour l'année 2017" > sortie.txt
+grep "Location" 2017*.ann | wc -l >> sortie.txt
+echo "pour l'année 2018" > sortie.txt
+grep "Location" 2018*.ann | wc -l >> sortie.txt
