eba855905a2a49849d388838e370e186cb0b76ca
Srharahaad <sunshineflash.mlp@gmail.com>
2022-11-30 10:40:51 +0100

    Résultat fin première feuille exercices urls

--- traitement_url_base.sh
+++ traitement_url_base.sh
@@ -6,26 +6,34 @@
 #---SETUP---
 if [ $# -eq 2 ]
 then
-	echo "Traitement en cours..."
+	echo "Traitement en cours...";
 else
-	echo "Il y a un problème avec les arguments. Vérifiez bien qu'il y a seulement deux arguments, le premier étant le fichier d'urls et le second la sortie."
-	exit
+	echo "Il y a un problème avec les arguments. Vérifiez bien qu'il y a seulement deux arguments, le premier étant le fichier d'urls et le second la sortie.";
+	exit;
 fi
-fichier_urls=$1 # le fichier d'URL en entrée
-fichier_tableau=$2 # le fichier HTML en sortie
+fichier_urls=$1; # le fichier d'URL en entrée
+fichier_tableau=$2; # le fichier HTML en sortie
+search="(I|i)mmigration"; # la regex de recherche pour le mot
 
 #---TRAITEMENT---
-echo "<html><head><meta charset=\"UTF-8\" /><title>Tableaux</title></head><body><table><tr><th>ligne</th><th>code</th><th>URL</th></tr>" > $fichier_tableau
+echo "<html><head><meta charset=\"UTF-8\" /><title>Tableaux</title></head><body><table><tr><th>ligne</th><th>code</th><th>encodage</th><th>word count</th><th>URL</th></tr>" > $fichier_tableau
 linenum=1;
-while read -r line;
+while read -r line
 do
 	echo "<tr><td>$linenum</td><td>" >> $fichier_tableau;
 	curl -I -s $line | egrep "^HTTP" | egrep -o "[0-9]{3}" >> $fichier_tableau;
-	echo "</td><td>$line</td></tr>" >> $fichier_tableau
+	echo "</td><td>" >> $fichier_tableau;
+	charset=$(curl -I -s $line | grep -Po "charset=[\w-]+" | grep -Po "=[\w-]+" | egrep -o "[^=]+");
+	if [ ! $charset ]
+	then
+		charset="Utf-8";
+	fi
+	echo $charset >> $fichier_tableau;
+	echo "</td><td>" >> $fichier_tableau;
+	lynx -dump -nolist -assume_charset=$charset $line | egrep -c $search >> $fichier_tableau;
+	echo "</td><td>$line</td></tr>" >> $fichier_tableau;
 	linenum=$((linenum+1));
 done < $fichier_urls
-echo "</table></body></html>" >> $fichier_tableau
-echo "Traitement terminé."
-exit
-
-#Continuer à l'ex 4
+echo "</table></body></html>" >> $fichier_tableau;
+echo "Traitement terminé.";
+exit;
